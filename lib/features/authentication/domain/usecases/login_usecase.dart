607ac640b61e89f208f5930d3c8180e730b5d6b7
Ahmed Shahawy <ahmed.shahawy@yahoo.com>
2022-09-20 16:50:07 +0200

    add base usecase

--- lib/features/authentication/domain/usecases/login_usecase.dart
+++ lib/features/authentication/domain/usecases/login_usecase.dart
@@ -1,15 +1,17 @@
 import 'package:alo_booking_app/core/exceptions/exceptions.dart';
+import 'package:alo_booking_app/core/usecase/base_usecase.dart';
 import 'package:alo_booking_app/features/authentication/domain/entities/auth.dart';
 import 'package:alo_booking_app/features/authentication/domain/entities/login.dart';
 import 'package:alo_booking_app/features/authentication/domain/repository/base_home_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class LoginUseCase {
+class LoginUseCase extends BaseUseCase<Login, Auth> {
   final BaseHomeRepository baseHomeRepository;
 
   LoginUseCase(this.baseHomeRepository);
 
-  Future<Either<PrimaryServerException, Login>> call(Auth auth) async {
-    return await baseHomeRepository.login(auth);
+  @override
+  Future<Either<PrimaryServerException, Login>> call(Auth parameters) async {
+    return await baseHomeRepository.login(parameters);
   }
 }
